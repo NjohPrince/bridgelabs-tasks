2c9a990145821d964356e01db80a3f7a19130374
NjohPrince <jufredprince@gmail.com>
2022-01-25 09:42:15 +0100

    routes setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Routes, Route } from 'react-router';
+
+import LandingPage from './containers/Landing.page.jsx';
+import RegisterPage from './containers/auth/Register.page.jsx';
+import LoginPage from './containers/auth/Login.page.jsx';
+import ErrorPage from './containers/404/Error.page.jsx';
 
 const App = () => {
   return (
     <div className="app">
-      Application Setup
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<LandingPage />} />
+
+          {/** Auth Routes */}
+          <Route exact path="/auth/login" element={<LoginPage />} />
+          <Route exact path="/auth/register" element={<RegisterPage />} />
+
+          {/** Error - 404 Route */}
+          <Route element={<ErrorPage />} />
+        </Routes>
+      </Router>
     </div>
-  )
-}
+  );
+};
 
 export default App;
---
+++ src/containers/auth/Register.page.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const RegisterPage = () => {
+  return <div>Registration Page</div>;
+};
+
+export default RegisterPage;
