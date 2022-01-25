07a36716ce55f3c98afce151ae4562e9b348d988
NjohPrince <jufredprince@gmail.com>
2022-01-25 10:02:53 +0100

    header setup

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { AppName } from '../../utils/appName/AppName.js';
+import { NavLinks } from '../../utils/links/Links.js';
 
 const Header = () => {
     return (
-        <div>
-            Header
+        <div className="navbar flex a-j-space-between">
+            <div className="logo">
+                <h2>{AppName}</h2>
+            </div>
+            <ul className="menu flex">
+                {
+                    NavLinks.length > 0 && NavLinks.map((links, index) => {
+                        return (
+                            <Link to={links.path} key={index + links.name}>{links.name}</Link>
+                        )
+                    })
+                }
+            </ul>
         </div>
     );
 };
--- src/containers/Landing.page.jsx
+++ src/containers/Landing.page.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
+import Header from '../components/header/Header.jsx';
+
 const LandingPage = () => {
-  return <div>Landing Page</div>;
+  return (
+    <React.Fragment>
+      <Header />
+      <div>
+        Landing Content
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default LandingPage;
