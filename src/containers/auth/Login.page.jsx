8e20136b0d8c0959dd6024b2b8de3ea845a623ac
NjohPrince <jufredprince@gmail.com>
2022-01-25 11:12:51 +0100

    auth pages completed styles

--- src/containers/auth/Login.page.jsx
+++ src/containers/auth/Login.page.jsx
@@ -1,21 +1,44 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './auth.css';
 
 const LoginPage = () => {
+  const preLoadParticles = Array(30).fill(0);
+
   return (
-    <div className="auth-container">
+    <div className="auth-container relative flex default-container a-j-center">
+      {preLoadParticles.length > 0 &&
+        preLoadParticles.map(() => {
+          return <div class="particle"></div>;
+        })}
       <div className="auth">
         <div className="form-control">
-          <i className="fas fa-envelope" aria-hidden="true"></i>
-          <input type="text" placeholder="Email" />
+          <div className="head">
+            <h1>Login</h1>
+          </div>
         </div>
-        <div className="form-control">
+        <div className="form-control flex a-j-center">
+          <i className="fas fa-envelope" aria-hidden="true"></i>
+          <input type="email" placeholder="Email" />
+        </div>
+        <div className="form-control flex a-j-center">
           <i className="fas fa-lock" aria-hidden="true"></i>
-          <input type="text" placeholder="Password" />
+          <input type="password" placeholder="Password" />
         </div>
         <div className="form-control">
           <button type="submit">Login</button>
+        </div>
+        <div className="form-control">
+          <h4 style={{ marginTop: '1rem' }}>
+            Don't have an account?{' '}
+            <Link
+              style={{ fontWeight: '700', color: 'var(--main-color)' }}
+              to="/auth/register"
+            >
+              Register
+            </Link>
+          </h4>
         </div>
       </div>
     </div>
--- src/containers/auth/Register.page.jsx
+++ src/containers/auth/Register.page.jsx
@@ -1,7 +1,56 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './auth.css';
 
 const RegisterPage = () => {
-  return <div>Registration Page</div>;
+  const preLoadParticles = Array(30).fill(0);
+
+  return (
+    <div className="auth-container relative flex default-container a-j-center">
+      {preLoadParticles.length > 0 &&
+        preLoadParticles.map(() => {
+          return <div class="particle"></div>;
+        })}
+      <div className="auth">
+        <div className="form-control">
+          <div className="head">
+            <h1>Register</h1>
+          </div>
+        </div>
+        <div className="form-control flex a-j-center">
+          <i className="fas fa-user" aria-hidden="true"></i>
+          <input type="text" placeholder="Enter Your Full Name" />
+        </div>
+        <div className="form-control flex a-j-center">
+          <i className="fas fa-envelope" aria-hidden="true"></i>
+          <input type="email" placeholder="Your Email" />
+        </div>
+        <div className="form-control flex a-j-center">
+          <i className="fas fa-lock" aria-hidden="true"></i>
+          <input type="password" placeholder="Password" />
+        </div>
+        <div className="form-control flex a-j-center">
+          <i className="fas fa-lock" aria-hidden="true"></i>
+          <input type="password" placeholder="Confirm Password" />
+        </div>
+        <div className="form-control">
+          <button type="submit">Register</button>
+        </div>
+        <div className="form-control">
+          <h4 style={{ marginTop: '1rem' }}>
+            Already have an account?{' '}
+            <Link
+              style={{ fontWeight: '700', color: 'var(--main-color)' }}
+              to="/auth/login"
+            >
+              Login
+            </Link>
+          </h4>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default RegisterPage;
