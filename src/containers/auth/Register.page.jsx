5d9ca0724c2c092904ccb4c5d95a7f161e2a9a32
NjohPrince <jufredprince@gmail.com>
2022-01-25 11:37:16 +0100

    registration data collection

--- src/containers/auth/Register.page.jsx
+++ src/containers/auth/Register.page.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import './auth.css';
@@ -6,13 +6,63 @@ import './auth.css';
 const RegisterPage = () => {
   const preLoadParticles = Array(30).fill(0);
 
+  const [erorrMessage, setErrorMessage] = useState('');
+  const [formData, setFormData] = useState({
+    fullname: '',
+    email: '',
+    password: '',
+    confirm_password: '',
+  });
+
+  const { fullname, email, password, confirm_password } = formData;
+  const onChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+    setErrorMessage('');
+  };
+
+  const validateEmail = (email) => {
+    return email.match(
+      /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+  };
+  const handleSubmit = (e) => {
+    setErrorMessage('');
+    e.preventDefault();
+    if (
+      email === '' ||
+      password === '' ||
+      fullname === '' ||
+      confirm_password === ''
+    ) {
+      setErrorMessage('Please fill out all fields!');
+      return;
+    }
+    if (fullname.length <= 2) {
+      setErrorMessage('Name length too short!');
+      return;
+    }
+    if (!validateEmail(email)) {
+      setErrorMessage('Email not valid!');
+      return;
+    }
+    if (password.length < 6) {
+      setErrorMessage('Password: enter atleast 6 characters.');
+      return;
+    }
+    if (confirm_password !== password) {
+      setErrorMessage('Passwords must match!');
+      return;
+    }
+    console.log(email, password);
+  };
+
   return (
     <div className="auth-container relative flex default-container a-j-center">
       {preLoadParticles.length > 0 &&
         preLoadParticles.map(() => {
           return <div class="particle"></div>;
         })}
-      <form className="auth">
+      <form onSubmit={handleSubmit} className="auth">
         <div className="form-control">
           <div className="head">
             <h1>Register</h1>
@@ -20,19 +70,19 @@ const RegisterPage = () => {
         </div>
         <div className="form-control flex a-j-center">
           <i className="fas fa-user" aria-hidden="true"></i>
-          <input type="text" placeholder="Enter Your Full Name" />
+          <input onChange={onChange} name="fullname" value={fullname} type="text" placeholder="Enter Your Full Name" />
         </div>
         <div className="form-control flex a-j-center">
           <i className="fas fa-envelope" aria-hidden="true"></i>
-          <input type="text" placeholder="Your Email" />
+          <input onChange={onChange} name="email" value={email} type="text" placeholder="Your Email" />
         </div>
         <div className="form-control flex a-j-center">
           <i className="fas fa-lock" aria-hidden="true"></i>
-          <input type="password" placeholder="Password" />
+          <input onChange={onChange} name="password" value={password} type="password" placeholder="Password" />
         </div>
         <div className="form-control flex a-j-center">
           <i className="fas fa-lock" aria-hidden="true"></i>
-          <input type="password" placeholder="Confirm Password" />
+          <input onChange={onChange} name="confirm_password" value={confirm_password} type="password" placeholder="Confirm Password" />
         </div>
         <div className="form-control">
           <button type="submit">Register</button>
@@ -47,6 +97,9 @@ const RegisterPage = () => {
               Login
             </Link>
           </h4>
+        </div>
+        <div className="form-control">
+          {erorrMessage && <h4 className="error">{erorrMessage}</h4>}
         </div>
       </form>
     </div>
