b38f1121683ef7bf1bc24cd834225e49ed6fd642
NjohPrince <jufredprince@gmail.com>
2022-01-25 09:27:02 +0100

    app links and app name config

---
+++ src/utils/links/Links.js
@@ -0,0 +1,18 @@
+export const NavLinks = [
+    {
+        name: 'Home',
+        path: '/'
+    },
+    {
+        name: 'About Us',
+        path: '/about-us'
+    },
+    {
+        name: 'Blog',
+        path: '/blog'
+    },
+    {
+        name: 'Pricing',
+        path: '/pricing'
+    }
+];
